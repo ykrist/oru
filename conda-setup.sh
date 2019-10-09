a8df18970dd2c6f974ebdf26c69d27b909ec056e
yanni_rist <yanni555rist@gmail.com>
2019-10-09 14:29:40 +1000

    add script to install into conda envs

---
+++ conda-setup.sh
@@ -0,0 +1,54 @@
+#/bin/bash
+set -e
+ORU_ROOT_DIR=$(realpath $0 | xargs dirname)
+ORU_BIN_DIR=$ORU_ROOT_DIR/bin
+ORU_ENV_FILE_NAME="oru_path_env.sh"
+ACTIVATE_DIR=${CONDA_PREFIX}/etc/conda/activate.d
+ACTIVATE_FILE=${ACTIVATE_DIR}/${ORU_ENV_FILE_NAME}
+DEACTIVATE_DIR=${CONDA_PREFIX}/etc/conda/deactivate.d
+DEACTIVATE_FILE=${DEACTIVATE_DIR}/${ORU_ENV_FILE_NAME}
+
+function install {
+  while true ; do
+    read -sp "Install into $CONDA_PREFIX? [y/n] " RESPONSE
+    echo
+    case $RESPONSE in
+      y) break ;;
+      n) exit ;;
+    esac
+  done
+  conda develop .
+  mkdir -p $ACTIVATE_DIR $DEACTIVATE_DIR
+
+  ACTIVATE_CMD="export PATH=\$PATH:${ORU_BIN_DIR}:"
+  DEACTIVATE_CMD="export PATH=\$(echo \$PATH | sed 's!:${ORU_BIN_DIR}:!!')"
+
+  echo -e "#!/bin/sh\n${ACTIVATE_CMD}" >  $ACTIVATE_FILE
+  echo -e "#!/bin/sh\n${DEACTIVATE_CMD}" > $DEACTIVATE_FILE
+  echo "successfully installed."
+};
+
+function uninstall {
+  conda develop -u $ORU_ROOT_DIR
+  rm -f $ACTIVATE_FILE
+  rm -f $DEACTIVATE_FILE
+  echo "successfully uninstalled."
+}
+
+if [[ -z $CONDA_PREFIX ]] ; then
+  echo "Error: not in a conda environment."
+  exit 1
+fi
+
+case $1 in
+  "install") install
+    ;;
+  "uninstall") uninstall
+    ;;
+  *) echo "Usage: ${0} (install | uninstall)"
+esac
+
+
+
+exit
+
--- setup.py
+++
@@ -1,3 +0,0 @@
-import setuptools
-
-setuptools.setup(name='oru', version='0.0', packages=['oru'], install_requires=['dacite', 'gurobi'])
