c432c6e508f84ce11a2a4c962b39157db0ac587d
yanni_rist <yanni555rist@gmail.com>
2019-10-14 20:55:49 +1000

    forgot to add

---
+++ oru/viz.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pygraphviz
+from functools import wraps
+
+class Interval:
+    def __init__(self,a,b):
+        assert a < b
+        self.start = a
+        self.end = b
+
+    def __contains__(self, val):
+        return self.start <= val <= self.end
+
+    def __matmul__(self, other):
+        return Rectangle(self.start, other.start, self.end, other.end)
+
+class Rectangle:
+    def __init__(self, x1,y1,x2=None,y2=None):
+        if x2 == y2 == None:
+            x2,y2 = x1,y1
+            x1 = y1 = 0
+
+        self.x_bounds = Interval(x1, x2)
+        self.y_bounds = Interval(y1, y2)
+
+    def __contains__(self, point):
+        return point[0] in self.x_bounds and point[1] in self.y_bounds
+
+    @property
+    def start(self):
+        return np.array([self.x_bounds.start, self.y_bounds.start])
+
+    @property
+    def end(self):
+        return np.array([self.x_bounds.end, self.y_bounds.end])
+
+
+def check_domain(func):
+    @wraps(func)
+    def wrapper(self, arg, **kwargs):
+        if arg not in self.domain:
+            raise ValueError(f"{str(arg)} is not in the domain.")
+        return func(arg, **kwargs)
+    return wrapper
+
+class BaseTransform:
+    pass
+
+class LinearTransform(BaseTransform):
+    def __init__(self, src_interval : Interval, dest_interval : Interval):
+        a,b = src_interval.start, src_interval.end
+        c,d = dest_interval.start, dest_interval.end
+        self.scale = (d-c)/(b-a)
+        self.offset = (b*c-a*d)/(b-a)
+        self.domain = src_interval
+
+    @check_domain
+    def __call__(self, val):
+        assert val in self.domain
+        return self.scale*val + self.offset
+
+class RectangularTransform(BaseTransform):
+    def __init__(self, source_rectangle : Rectangle, dest_rectangle : Rectangle):
+        ax1,ay1 = source_rectangle.start
+        ax2,ay2 = source_rectangle.end
+        bx1,by1 = dest_rectangle.start
+        bx2,by2 = dest_rectangle.end
+        x_transform = LinearTransform((ax1,ax2), (bx1,bx2))
+        y_transform = LinearTransform((ay1,ay2), (by1,by2))
+
+        self.scale = np.array([x_transform.scale, y_transform.scale])
+        self.offset = np.array([x_transform.offset, y_transform.offset])
+        self.domain = source_rectangle
+
+    @check_domain
+    def __call__(self, point):
+        return self.scale*point + self.offset
+
+
+
+
