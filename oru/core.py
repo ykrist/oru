dff9fc30f32038cc4fa87d14d784afbd9212e2d5
yanni_rist <yanni555rist@gmail.com>
2020-10-05 17:19:11 +1000

    better repr

--- oru/core.py
+++ oru/core.py
@@ -49,7 +49,7 @@ class frozendict(Mapping[_K,_V]):
         return len(self._dict)
 
     def __repr__(self):
-        return '<%s %r>' % (self.__class__.__name__, self._dict)
+        return f'{self.__class__.__name__}({self._dict!r})'
 
     def __hash__(self):
         if self._hash is None:
